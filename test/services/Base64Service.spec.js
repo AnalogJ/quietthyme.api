7921e028305bf77e0ce58a304c1acf97d318979d
Jason Kulatunga <jk17@ualberta.ca>
2017-04-09 21:38:35 -0700

    update debug statement.

--- test/services/Base64Service.spec.js
+++ test/services/Base64Service.spec.js
@@ -1,6 +1,49 @@
 var should = require('should');
 var Base64Service = require('../../src/services/Base64Service')
 //this is just simple integration testing
-describe.skip('Base64Service', function () {
-
+describe('Base64Service', function () {
+    describe('encode', function(){
+        it('should encode correctly', function () {
+            Base64Service.encode('').should.eql('')
+            Base64Service.encode("f").should.eql("Zg==")
+            Base64Service.encode("fo").should.eql("Zm8=")
+            Base64Service.encode("foo").should.eql("Zm9v")
+            Base64Service.encode("foob").should.eql("Zm9vYg==")
+            Base64Service.encode("fooba").should.eql("Zm9vYmE=")
+            Base64Service.encode("foobar").should.eql("Zm9vYmFy")
+        })
+    })
+    describe('decode', function(){
+        it('should decode correctly', function () {
+            Base64Service.decode('').should.eql('')
+            Base64Service.decode("Zg==").should.eql("f")
+            Base64Service.decode("Zm8=").should.eql("fo")
+            Base64Service.decode("Zm9v").should.eql("foo")
+            Base64Service.decode("Zm9vYg==").should.eql("foob")
+            Base64Service.decode("Zm9vYmE=").should.eql("fooba")
+            Base64Service.decode("Zm9vYmFy").should.eql("foobar")
+        })
+    })
+    describe('urlEncode', function(){
+        it('should encode correctly', function () {
+            Base64Service.urlEncode('').should.eql('')
+            Base64Service.urlEncode("f").should.eql("Zg")
+            Base64Service.urlEncode("fo").should.eql("Zm8")
+            Base64Service.urlEncode("foo").should.eql("Zm9v")
+            Base64Service.urlEncode("foob").should.eql("Zm9vYg")
+            Base64Service.urlEncode("fooba").should.eql("Zm9vYmE")
+            Base64Service.urlEncode("foobar").should.eql("Zm9vYmFy")
+        })
+    })
+    describe('urlDecode', function(){
+        it('should decode correctly', function () {
+            Base64Service.urlDecode('').should.eql('')
+            Base64Service.urlDecode("Zg==").should.eql("f")
+            Base64Service.urlDecode("Zm8=").should.eql("fo")
+            Base64Service.urlDecode("Zm9v").should.eql("foo")
+            Base64Service.urlDecode("Zm9vYg==").should.eql("foob")
+            Base64Service.urlDecode("Zm9vYmE=").should.eql("fooba")
+            Base64Service.urlDecode("Zm9vYmFy").should.eql("foobar")
+        })
+    })
 })
--- test/services/JWTokenService.spec.js
+++ test/services/JWTokenService.spec.js
@@ -1,6 +1,29 @@
 var should = require('should');
 var JWTokenService = require('../../src/services/JWTokenService')
 //this is just simple integration testing
-describe.skip('JWTokenService', function () {
+describe('JWTokenService', function () {
+    describe('issue', function(){
+        it('should generate a valid token', function (done) {
+            var token = JWTokenService.issue({
+                payload: "data"
+            })
 
+            JWTokenService.verify(token)
+                .then(function(decoded){
+                    decoded.payload.should.be.eql("data")
+                })
+                .then(done, done);
+        })
+    })
+
+
+    describe('verify', function(){
+        it('should raise an error if decoding an invalid token', function (done) {
+            JWTokenService.verify('123456')
+                .fail(function(decoded){
+                    decoded.should.be.an.Error
+                })
+                .then(done, done);
+        })
+    })
 })
