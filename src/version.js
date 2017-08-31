c2a3d40ba00689c379b4e239403a4e5d499c623c
Jason Kulatunga <jk17@ualberta.ca>
2017-08-30 18:28:30 -0700

    also print the event data in beta.

--- src/version.js
+++ src/version.js
@@ -5,6 +5,9 @@ module.exports.handler = (event, context, callback) => {
   var versionInfo = {
     deploySha: nconf.get('DEPLOY_SHA'),
   };
+  if (nconf.get('STAGE') == 'beta') {
+    versionInfo.event = event
+  }
 
   return callback(null, versionInfo);
 };
