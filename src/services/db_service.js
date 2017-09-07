dba7f95558a01dd19989260dbc14de447b2e866e
Jason Kulatunga <jk17@ualberta.ca>
2017-09-07 09:18:31 -0700

    handle password updates and catalog updates.

--- src/services/db_service.js
+++ src/services/db_service.js
@@ -185,7 +185,7 @@ dbService.updateUser = function(user_id, update_data, return_values) {
 
   //valid auth token,
   //next lets filter the updated user object so it only contains properties that we allow users to update via the API.
-  var whitelist = ["name", "library_uuid", "catalog_token"];
+  var whitelist = ["name", "library_uuid", "catalog_token", "password_hash"];
 
 
   var update_data = filter(update_data, whitelist);
--- src/user.js
+++ src/user.js
@@ -21,6 +21,12 @@ UserEndpoint.router = function(event, context, cb){
   else if(event.path.action == 'update' && event.method == 'POST'){
     UserEndpoint.update(event, context, cb)
   }
+  else if(event.path.action == 'catalog' && event.method == 'POST'){
+    UserEndpoint.catalogRegen(event, context, cb)
+  }
+  else if(event.path.action == 'password' && event.method == 'POST'){
+    UserEndpoint.password(event, context, cb)
+  }
   else{
     Utilities.errorHandler(cb)(new Error(`Unknown API endpoint: ${event.path.action}`))
   }
@@ -50,6 +56,71 @@ UserEndpoint.update = function (event, context, cb){
           }),
         };
       });
+    })
+    .then(Utilities.successHandler(cb))
+    .fail(Utilities.errorHandler(cb))
+    .done();
+}
+
+
+UserEndpoint.catalogRegen = function (event, context, cb){
+  q.all([JWTokenService.verify(event.token), SecurityService.generate_catalog_token()])
+    .spread(function(auth, new_catalog_token) {
+
+      //can only be used to update teh following fields:  "name", "library_uuid", "catalog_token", all other fields ignored.
+      return DBService.updateUser(
+        auth.uid,
+        {catalog_token: new_catalog_token},
+        true
+      )
+        .then(function(user_data) {
+          //return the new token
+          debug('Updated token: %o', user_data);
+          return {
+            token: JWTokenService.issue({
+              uid: user_data.uid,
+              plan: user_data.plan,
+              catalog_token: user_data.catalog_token,
+              name: user_data.name,
+              email: user_data.email,
+            }),
+          };
+        });
+    })
+    .then(Utilities.successHandler(cb))
+    .fail(Utilities.errorHandler(cb))
+    .done();
+}
+
+UserEndpoint.password = function (event, context, cb){
+  JWTokenService.verify(event.token)
+    .then(function(auth) {
+      return DBService.findUserById(auth.uid)
+        .then(function(user){
+          //validate that the old password is correct
+          return SecurityService.compare_password(
+            event.body.oldPassword,
+            user.password_hash
+          ).then(function(matched) {
+            if (matched) {
+              return user;
+            } else {
+              throw new Error('Email or Password is incorrect');
+            }
+          });
+        })
+        .then(function(){
+          //the old password matches, now lets update the user password.
+          return SecurityService.hash_password(event.body.newPassword)
+        })
+        .then(function(hashed_password){
+          //can only be used to update teh following fields:  "name", "library_uuid", "catalog_token", all other fields ignored.
+          return DBService.updateUser(
+            auth.uid,
+            {password_hash: hashed_password},
+            true
+          )
+        })
     })
     .then(Utilities.successHandler(cb))
     .fail(Utilities.errorHandler(cb))
