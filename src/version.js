97d675dd1aed503b8568cf8d4693d3825d3ec93b
Jason Kulatunga <jk17@ualberta.ca>
2017-09-16 17:33:01 -0700

    cleanup versipno handler.

--- src/version.js
+++ src/version.js
@@ -6,16 +6,7 @@ var GlobalHandler = require('./common/global_handler');
 
 var VersionHandler = module.exports
 
-VersionHandler.router = GlobalHandler.wrap(function(event, context, cb) {
-  debug('AuthEndpoint router event: %o', event);
-  if (event.pathParameters.action == 'version' && event.httpMethod == 'GET') {
-    VersionHandler.handler(event, context, cb);
-  } else {
-    Utilities.errorHandler(cb)(
-      new Error(`Unknown API endpoint: ${event.pathParameters.action}`)
-    );
-  }
-})
+VersionHandler.router = GlobalHandler.wrap(VersionHandler.handler)
 
 
 VersionHandler.handler = function(event, context, callback){
